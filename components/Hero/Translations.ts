749bf4bb110cfa30c708c8fce558eedc0f489628
Diego Costa Tech <Diegoe.r.c07@gmail.com>
2025-12-02 23:47:53 -0300

    Update Translations.ts

--- components/Hero/Translations.ts
+++ components/Hero/Translations.ts
@@ -4,15 +4,15 @@ interface HeroTranslations {
 }
 
 const translations: { [language: string]: HeroTranslations } = {
-en: {
-  title: 'Solution-Oriented Developer',
-  description: 'Dedicated to building efficient, scalable, and modern software applications.',
-},
-
-'pt-br': {
-  title: 'Desenvolvedor Orientado a Soluções',
-  description: 'Dedicado a construir aplicações de software eficientes, escaláveis e modernas.',
-},
-
+    en: {
+      title: 'Solution-Oriented Developer',
+      description: 'Dedicated to building efficient, scalable, and modern software applications.',
+    },
+    
+    'pt-br': {
+      title: 'Desenvolvedor Orientado a Soluções',
+      description: 'Dedicado a construir aplicações de software eficientes, escaláveis e modernas.',
+    },
+};
 
 export default translations;
